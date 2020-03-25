be6d4e40bb5b8b8302281e7706624eb8f41fb196
Sota <sooota09@gmail.com>
2020-03-25 21:31:59 +0900

    [Finish] Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
+  def home
+  end
+
   def index
     @books = Book.all
+    @book = Book.new
+
   end
 
   def show
@@ -11,35 +16,37 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-  # def create
-  #   book = Book.new(book_params)
-  #   book.save
-  #   redirect_to books_path
-  # end
-
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
-      redirect_to controller: :books, action: :show
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
     else
-      render "new"
+      render :index
     end
   end
 
   def edit
-    @book = Book.find(params[:id]) 
+    @book = Book.find(params[:id])
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+    else
+      render :show
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path   
+    if book.destroy
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
+    else
+      flash[:alert]
+      render :index
+    end
   end
 
   private
